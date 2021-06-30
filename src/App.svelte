2e94d8d371923ee354ebe044be173754b9a0e9cd
Guibolt <guilhermedosreiss@gmail.com>
2021-06-30 16:13:58 -0300

    Loops

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,32 @@
 <script>
-	 let firstName = 'John'
-	 let lastName = 'Lennon'
-	let color = 'black'
-	const handleClick = () =>{
-		name =' novo nome'
-	}
-	const handleInput = (event) => {
-		name = event.target.value
 
-	}
+	let people = [
+		{ name: 'john', beltColor: 'black', age: 25, id: 1 },
+		{ name: 'mario', beltColor: 'orange', age: 45, id: 2 },
+		{ name: 'lones', beltColor: 'blue', age: 39, id: 3 }
+	]
 
-	$: fullName = `${firstName} ${lastName}`
-	$: {
-		console.log(color)
-		console.log(fullName)
+	const deletePerson = (id) =>{
+		people = people.filter((person) => person.id != id)
 	}
 </script>
 
 <main>
-	<p style="color: {color}">name: {fullName} -{color} cor</p>
-	
-	<input type="text" bind:value={color}>
-	<input type="text" bind:value={lastName}>
-	<input type="text" bind:value={firstName}>
-
+	{#each people as person (person.id)}
+	<div>
+		<h4>
+			{person.name}
+		</h4>
+		<p>{person.age} years old, {person.beltColor} belt.</p>
+		<button on:click="{() => deletePerson(person.id)}"> Delete</button>
+	</div>
+	{:else}
+	<p>No data to show</p>
+	{/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+	
 
 	@media (min-width: 640px) {
 		main {
